257b0d0e8777ecdc2bc66f168bbf97726d9afa89
RaulCaliman <211803244+Darian-Raul-Caliman@users.noreply.github.com>
2026-03-24 22:11:02 +0200

    Laborator 3 C) + Conventie nume

--- src/main/java/ro/ulbs/proiectaresoftware/lab3/Main.java
+++ src/main/java/ro/ulbs/proiectaresoftware/lab3/Main.java
@@ -1,49 +1,50 @@
 package ro.ulbs.proiectaresoftware.lab3;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.*;
-
-import static java.lang.System.in;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         String fileName = "C:\\Users\\raulc\\IdeaProjects\\LaboratoareSoftware\\src\\main\\java\\ro\\ulbs\\proiectaresoftware\\lab3\\text.txt";
+        String outFileName = "C:\\Users\\raulc\\IdeaProjects\\LaboratoareSoftware\\src\\main\\java\\ro\\ulbs\\proiectaresoftware\\lab3\\out.txt";
         Path path = Paths.get(fileName);
+        Path outPath = Paths.get(outFileName);
+
         List<String> text = citireFisier(path);
-        modificari(text);
-        modificari2(text);
-
-
-
+        adaugaRandNou(text);
+        separaDupaPunct(text);
+        scriereFisier(text, outPath);
     }
 
-    public static void modificari(List<String> ListaNoua) {
-
-        for (int i = 0; i < ListaNoua.size(); i++) {
-            String liniaCurenta = ListaNoua.get(i);
-            ListaNoua.set(i, liniaCurenta + "\n");
+    public static void adaugaRandNou(List<String> listaNoua) {
+        for (int i = 0; i < listaNoua.size(); i++) {
+            String liniaCurenta = listaNoua.get(i);
+            listaNoua.set(i, liniaCurenta + "\n");
         }
+
         System.out.println("----------------------Textul rezultat: \n");
-        for (String linie : ListaNoua) {
+        for (String linie : listaNoua) {
             System.out.print(linie);
         }
     }
 
-    public static void modificari2(List<String> ListaNoua) {
-        for (int i = 0; i < ListaNoua.size(); i++) {
-            String liniaCurenta = ListaNoua.get(i);
+    public static void separaDupaPunct(List<String> listaNoua) {
+        for (int i = 0; i < listaNoua.size(); i++) {
+            String liniaCurenta = listaNoua.get(i);
             String liniaModificata = liniaCurenta.replace(".", ".\n");
-            ListaNoua.set(i, liniaModificata);
-        }
-        System.out.println("--------------------Separare dupa punct :  \n");
-        for (String linie : ListaNoua) {
-            System.out.println(linie);
+            listaNoua.set(i, liniaModificata);
         }
 
+        System.out.println("--------------------Separare dupa punct :  \n");
+        for (String linie : listaNoua) {
+            System.out.println(linie);
+        }
     }
 
     public static List<String> citireFisier(Path path) {
@@ -54,5 +55,14 @@ public class Main {
             System.out.println("Eroare la citire: " + e.getMessage());
         }
         return lines;
+    }
+
+    public static void scriereFisier(List<String> iesire, Path path) throws IOException {
+        try (BufferedWriter writer = Files.newBufferedWriter(path)) {
+            for (String linie : iesire) {
+                writer.write(linie);
+                writer.newLine();
+            }
+        }
     }
 }
